70b67e4c85efc16b41ec55f286a7737aad2f61fa
Felipe Lira <filipi6219@gmail.com>
2024-01-13 11:00:27 -0300

    feat: fazer UserLoginController retornar badRequest se password nao for informado

--- src/presentation/controllers/user-login-controller.ts
+++ src/presentation/controllers/user-login-controller.ts
@@ -3,5 +3,6 @@ import { badRequest } from '@/presentation/helpers/http';
 export default class UserLoginController {
   async handle(request: any) {
     if (!request.username) return badRequest(new Error('Missing param: username'));
+    if (!request.password) return badRequest(new Error('Missing param: password'));
   }
 }
