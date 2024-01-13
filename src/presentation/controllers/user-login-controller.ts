79f809c08415e099582d533a56d1184845743fdb
Felipe Lira <filipi6219@gmail.com>
2024-01-13 11:09:33 -0300

    feat: fazer UserLoginController chamar userLoginUsecase corretamente

--- src/presentation/controllers/user-login-controller.ts
+++ src/presentation/controllers/user-login-controller.ts
@@ -1,8 +1,13 @@
 import { badRequest } from '@/presentation/helpers/http';
 
 export default class UserLoginController {
+  constructor(
+    private readonly userLoginUsecase: any
+  ) {}
+
   async handle(request: any) {
     if (!request.username) return badRequest(new Error('Missing param: username'));
     if (!request.password) return badRequest(new Error('Missing param: password'));
+    await this.userLoginUsecase.handle(request)
   }
 }
