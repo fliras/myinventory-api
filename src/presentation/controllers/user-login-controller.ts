73eb1e5052e8111268e07cbbab54fdc24063c240
Felipe Lira <filipi6219@gmail.com>
2024-01-13 11:14:49 -0300

    feat: fazer UserLoginController retornar serverError se userLoginUsecase quebrar

--- src/presentation/controllers/user-login-controller.ts
+++ src/presentation/controllers/user-login-controller.ts
@@ -1,13 +1,15 @@
-import { badRequest } from '@/presentation/helpers/http';
+import { badRequest, serverError } from '@/presentation/helpers/http';
 
 export default class UserLoginController {
-  constructor(
-    private readonly userLoginUsecase: any
-  ) {}
+  constructor(private readonly userLoginUsecase: any) {}
 
   async handle(request: any) {
-    if (!request.username) return badRequest(new Error('Missing param: username'));
-    if (!request.password) return badRequest(new Error('Missing param: password'));
-    await this.userLoginUsecase.handle(request)
+    try {
+      if (!request.username) return badRequest(new Error('Missing param: username'));
+      if (!request.password) return badRequest(new Error('Missing param: password'));
+      await this.userLoginUsecase.handle(request);
+    } catch (error) {
+      return serverError();
+    }
   }
 }
--- src/presentation/helpers/http.ts
+++ src/presentation/helpers/http.ts
@@ -2,3 +2,8 @@ export const badRequest = (error: Error) => ({
   statusCode: 400,
   data: error,
 });
+
+export const serverError = () => ({
+  statusCode: 500,
+  data: new Error('Internal server error'),
+});
