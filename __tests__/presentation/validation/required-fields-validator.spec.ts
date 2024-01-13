189a9f3f184f8f8770786e50c4751e89207c439e
Felipe Lira <filipi6219@gmail.com>
2024-01-13 17:19:31 -0300

    test: fazer RequiredFieldsValidator retornar undefined se o campo obrigatorio for informado

--- __tests__/presentation/validation/required-fields-validator.spec.ts
+++ __tests__/presentation/validation/required-fields-validator.spec.ts
@@ -3,8 +3,14 @@ import MissingParamError from '@/presentation/errors/missing-param-error';
 
 describe('RequiredFieldsValidator', () => {
   it('Should return MissingParamError if the specified field is not provided', () => {
-    const sut = new RequiredFieldsValidator('any-field');
+    const sut = new RequiredFieldsValidator('requiredField');
     const output = sut.validate({});
-    expect(output).toEqual(new MissingParamError('any-field'));
+    expect(output).toEqual(new MissingParamError('requiredField'));
+  });
+
+  it('Should return void if the specified field is provided', () => {
+    const sut = new RequiredFieldsValidator('requiredField');
+    const output = sut.validate({ requiredField: 'any-value' });
+    expect(output).toBe(undefined);
   });
 });
