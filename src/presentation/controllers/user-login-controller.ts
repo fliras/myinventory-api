68ca4706ed6053e05b498c78ddfc8735e24dad9a
Felipe Lira <filipi6219@gmail.com>
2024-01-13 11:35:43 -0300

    feat: fazer UserLoginController retornar badRequest se userLoginUsecase retornar um erro

--- src/presentation/controllers/user-login-controller.ts
+++ src/presentation/controllers/user-login-controller.ts
@@ -7,8 +7,9 @@ export default class UserLoginController {
     try {
       if (!request.username) return badRequest(new Error('Missing param: username'));
       if (!request.password) return badRequest(new Error('Missing param: password'));
-      const accessToken = await this.userLoginUsecase.handle(request);
-      return ok({ accessToken });
+      const result = await this.userLoginUsecase.handle(request);
+      if (result instanceof Error) return badRequest(result);
+      return ok({ accessToken: result });
     } catch (error) {
       return serverError();
     }
