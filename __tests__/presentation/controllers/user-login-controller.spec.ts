f96be29963baebe1b77538588d7d7f7099ba04fd
Felipe Lira <filipi6219@gmail.com>
2024-01-13 11:00:42 -0300

    test: fazer UserLoginController retornar badRequest se password nao for informado

--- __tests__/presentation/controllers/user-login-controller.spec.ts
+++ __tests__/presentation/controllers/user-login-controller.spec.ts
@@ -4,7 +4,13 @@ import { badRequest } from '@/presentation/helpers/http';
 describe('UserLoginController', () => {
   it('Should return badRequest if username is not provided', async () => {
     const sut = new UserLoginController();
-    const output = await sut.handle({});
+    const output = await sut.handle({ password: 'any-password' });
     expect(output).toEqual(badRequest(new Error('Missing param: username')));
+  });
+
+  it('Should return badRequest if password is not provided', async () => {
+    const sut = new UserLoginController();
+    const output = await sut.handle({ username: 'any-username' });
+    expect(output).toEqual(badRequest(new Error('Missing param: password')));
   });
 });
