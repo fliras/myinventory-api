948bdafb2a26796475c0680652b1455ff58dfbd8
Felipe Lira <filipi6219@gmail.com>
2024-01-13 21:24:12 -0300

    feat: fazer UserLoginUsecase retornar erro se CheckUserByUsernameRepository retornar false

--- src/domain/usecases/user-login-usecase.ts
+++ src/domain/usecases/user-login-usecase.ts
@@ -1,11 +1,13 @@
 import { CheckUserByUsername } from '@/data/contracts';
 import { UserLogin } from '@/domain/contracts';
+import { UserNotFoundError } from '@/domain/errors';
 
 export default class UserLoginUsecase implements UserLogin {
   constructor(private readonly checkUserByUsernameRepository: CheckUserByUsername) {}
 
   async handle(input: UserLogin.Input): Promise<UserLogin.Output> {
-    await this.checkUserByUsernameRepository.checkByUsername(input.username);
+    const userExists = await this.checkUserByUsernameRepository.checkByUsername(input.username);
+    if (!userExists) return new UserNotFoundError();
     return '';
   }
 }
