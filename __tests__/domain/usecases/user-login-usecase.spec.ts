b4863b05c3b19e85226df588de88420248719d92
Felipe Lira <filipi6219@gmail.com>
2024-01-13 21:05:13 -0300

    test: fazer UserLoginUsecase chamar CheckUserByUsernameRepository corretamente

---
+++ __tests__/domain/usecases/user-login-usecase.spec.ts
@@ -0,0 +1,37 @@
+import UserLoginUsecase from '@/domain/usecases/user-login-usecase';
+import { UserLogin } from '@/domain/contracts';
+
+class CheckUserByUsernameRepositoryStub {
+  async checkByUsername() {
+    return true;
+  }
+}
+
+type SutTypes = {
+  checkUserByUsernameRepository: CheckUserByUsernameRepositoryStub;
+  sut: UserLoginUsecase;
+};
+
+const makeSut = (): SutTypes => {
+  const checkUserByUsernameRepository = new CheckUserByUsernameRepositoryStub();
+  const sut = new UserLoginUsecase(checkUserByUsernameRepository);
+  return {
+    checkUserByUsernameRepository,
+    sut,
+  };
+};
+
+const mockInput = (): UserLogin.Input => ({
+  username: 'any-username',
+  password: 'any-password',
+});
+
+describe('UserLoginUsecase', () => {
+  it('Should call CheckUserByUsernameRepository with correct values', async () => {
+    const { checkUserByUsernameRepository, sut } = makeSut();
+    const checkUserSpy = jest.spyOn(checkUserByUsernameRepository, 'checkByUsername');
+    const input = mockInput();
+    await sut.handle(input);
+    expect(checkUserSpy).toHaveBeenCalledWith(input.username);
+  });
+});
