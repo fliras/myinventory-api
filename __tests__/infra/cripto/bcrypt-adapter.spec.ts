ff0e9ff0a0480f2e76d9d00236ef516b971c43ea
Felipe Lira <filipi6219@gmail.com>
2024-01-18 20:24:15 -0300

    test: fazer BcryptAdapter retornar false se bcrypt.compare retornar false

--- __tests__/infra/cripto/bcrypt-adapter.spec.ts
+++ __tests__/infra/cripto/bcrypt-adapter.spec.ts
@@ -3,13 +3,13 @@ import { BcryptAdapter } from '@/infra/cripto/bcrypt-adapter';
 
 jest.mock('bcryptjs', () => ({
   async compare(): Promise<boolean> {
-    return true
-  }
-}))
+    return true;
+  },
+}));
 
 const mockThrow = () => {
   throw new Error();
-}
+};
 
 describe('BcryptAdapter', () => {
   describe('compare()', () => {
@@ -17,14 +17,23 @@ describe('BcryptAdapter', () => {
       const sut = new BcryptAdapter();
       const bcryptSpy = jest.spyOn(bcrypt, 'compare');
       await sut.compare('any-text', 'any-hash');
-      expect(bcryptSpy).toHaveBeenCalledWith('any-text', 'any-hash')
-    })
+      expect(bcryptSpy).toHaveBeenCalledWith('any-text', 'any-hash');
+    });
 
     it('Should throw if bcrypt.compare throws', async () => {
       const sut = new BcryptAdapter();
       jest.spyOn(bcrypt, 'compare').mockImplementationOnce(mockThrow);
       const output = sut.compare('any-text', 'any-hash');
       expect(output).rejects.toThrow();
-    })
-  })
+    });
+
+    describe('Should return a boolean', () => {
+      it('when bcrypt.compare returns false', async () => {
+        const sut = new BcryptAdapter();
+        jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false);
+        const output = await sut.compare('any-text', 'any-hash');
+        expect(output).toBe(false);
+      });
+    });
+  });
 });
