701de52566add6e3fdb266628836709c9ae845c1
Felipe Lira <filipi6219@gmail.com>
2024-01-25 21:13:21 -0300

    test: fazer UsersRepository.loadByUsername retornar um usuario se ele existir

--- __tests__/infra/database/knex/repos/users-repository.spec.ts
+++ __tests__/infra/database/knex/repos/users-repository.spec.ts
@@ -1,6 +1,19 @@
 import db from '@/infra/database/knex/db';
 import UsersRepository from '@/infra/database/knex/repos/users-repository';
 
+const mockUser = async () => {
+  const [newUser] = await db('users')
+    .insert({
+      name: 'user',
+      username: 'username',
+      password: 'hashed-password',
+      status: 'ACTIVE',
+      role: 'OPERATOR',
+    })
+    .returning('*');
+  return newUser;
+};
+
 describe('UsersRepository', () => {
   beforeEach(async () => {
     await db('users').del();
@@ -15,6 +28,13 @@ describe('UsersRepository', () => {
       const sut = new UsersRepository();
       const user = await sut.loadByUsername('any-user');
       expect(user).toBeNull();
+    });
+
+    it('Should return an user if it exists', async () => {
+      const sut = new UsersRepository();
+      const mockedUser = await mockUser();
+      const loadedUser = await sut.loadByUsername(mockedUser.username);
+      expect(loadedUser?.id).toBe(mockedUser.user_id);
     });
   });
 });
