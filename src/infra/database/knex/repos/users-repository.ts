6f9400c602639a1f4fae266bc446afc16b46c628
Felipe Lira <filipi6219@gmail.com>
2024-01-25 21:13:05 -0300

    feat: fazer UsersRepository.loadByUsername retornar um usuario se ele existir

--- src/infra/database/knex/repos/users-repository.ts
+++ src/infra/database/knex/repos/users-repository.ts
@@ -1,7 +1,22 @@
 import { LoadUserByUsername } from '@/data/contracts';
+import db from '../db';
 
 export default class UsersRepository implements LoadUserByUsername {
   async loadByUsername(username: string): Promise<LoadUserByUsername.Output> {
-    return null;
+    const user = await db('users').first().where({ username });
+    return (user && this.map(user)) || null;
+  }
+
+  private map(user: any): LoadUserByUsername.Output {
+    return {
+      id: user.user_id,
+      name: user.name,
+      username: user.username,
+      hashedPassword: user.password,
+      status: user.status,
+      createdAt: user.created_at,
+      updatedAt: user.updated_at,
+      role: user.role,
+    };
   }
 }
