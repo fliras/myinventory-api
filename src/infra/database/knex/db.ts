28842e359ec8c4f32b4d05f2faa6df2cc47c5300
Felipe Lira <filipi6219@gmail.com>
2024-01-28 10:19:26 -0300

    refactor: remover redundancia desnecessaria

--- src/infra/database/knex/db.ts
+++ src/infra/database/knex/db.ts
@@ -1,16 +1,6 @@
 import knex from 'knex';
+import knexfile from './knexfile';
 
-const db = knex({
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
-  debug: false,
-});
+const db = knex(knexfile);
 
 export default db;
