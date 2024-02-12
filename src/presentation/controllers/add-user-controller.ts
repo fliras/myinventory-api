06e2c2a38b48b077e6a0f54622cfc36055d7492e
Felipe Lira <filipi6219@gmail.com>
2024-02-12 17:47:11 -0300

    feat: fazer AddUserController retornar badRequest se validation retornar um erro

--- src/presentation/controllers/add-user-controller.ts
+++ src/presentation/controllers/add-user-controller.ts
@@ -1,11 +1,12 @@
 import { Controller, HttpResponse, Validator } from '@/presentation/contracts';
-import { noContent } from '../helpers';
+import { badRequest, noContent } from '@/presentation/helpers';
 
 export class AddUserController implements Controller {
   constructor(private readonly validator: Validator) {}
 
   async handle(request: AddUserController.Request): Promise<HttpResponse> {
-    this.validator.validate(request);
+    const validation = this.validator.validate(request);
+    if (validation instanceof Error) return badRequest(validation);
     return noContent();
   }
 }
